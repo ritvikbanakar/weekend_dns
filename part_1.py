7789c0878f268320a68a99b27e909c02aa63aa75
Ritvik Banakar <ritvikbanakar15@gmail.com>
2023-07-06 17:52:09 -0700

    Send DNS query

--- part_1.py
+++ part_1.py
@@ -1,7 +1,23 @@
+# implementing a toy DNS resolver
+# expected behavior:
+# $ python3 resolve.py example.com
+# 93.184.216.34
+
 from dataclasses import dataclass
 import dataclasses
 import struct
 
+
+# DNS queries have 2 parts: a header and a question
+
+# First the DNS header consists of:
+# a) query ID
+# b) some flags (mostly ignored for poc)
+# c) 4 counts telling you how many records to expect in DNS packet
+    # num_questions
+    # num_answers
+    # num_authorities
+    # num_additionals
 
 @dataclass
 class DNSHeader:
@@ -12,34 +28,43 @@ class DNSHeader:
     num_authorities: int = 0
     num_additionals: int = 0
 
+# Next a DNS question has 3 fields:
+# a) name (like example.com)
+# b) type (like A)
+# c) class (always the same)
 
 @dataclass
 class DNSQuestion:
     name: bytes
-    type_: int
+    # type() is function
+    type_: int 
+    # class is reserved
     class_: int
 
-
+# Next these classes need to turn into byte strings
 def header_to_bytes(header):
     fields = dataclasses.astuple(header)
-    return struct.pack('!HHHHHH', *fields)
-
+    # there are 6 'H's because 6 fields from DNSHeader
+    # the ! at the beginning is for big endian for network packets
+    return struct.pack("!HHHHHH", *fields)
 
 def question_to_bytes(question):
-    return question.name + struct.pack('!HH', question.type_, question.class_)
+    return question.name + struct.pack("!HH", question.type_, question.class_)
 
-
+# Encodes the domain name
+# google.com -> b"\x06google\x03com\x00"
 def encode_dns_name(domain_name):
-    encoded = b''
-    for part in domain_name.encode('ascii').split(b'.'):
+    encoded = b""
+    for part in domain_name.encode("ascii").split(b'.'):
         encoded += bytes([len(part)]) + part
-    return encoded + b'\x00'
+        return encoded + b"\x00"
 
-
+# Building the DNS Query
 import random
+random.seed(1)
+
 TYPE_A = 1
 CLASS_IN = 1
-
 
 def build_query(domain_name, record_type):
     name = encode_dns_name(domain_name)
@@ -49,5 +74,29 @@ def build_query(domain_name, record_type):
     question = DNSQuestion(name=name, type_=record_type, class_=CLASS_IN)
     return header_to_bytes(header) + question_to_bytes(question)
 
-
+# time to test if this code works properly
 import socket
+query = build_query("www.example.com", 1)
+
+# create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# send our query to 8.8.8.8, port 53 (DNS port)
+sock.sendto(query, ("8.8.8.8", 53))
+
+# read the respond. UDP DNS responses are typically < 512 bytes
+response, _ = sock.recvfrom(1024)
+
+# by running tcpdump we can see our program making its DNS query
+print(response)
+
+
+
+
+
+
+
+
+
+
+
--- readme.txt
+++
@@ -1,1 +0,0 @@
-The .py files contain the exact same code as the .ipynb files. They're just there so that part_2 can import code from part_1 more easily
